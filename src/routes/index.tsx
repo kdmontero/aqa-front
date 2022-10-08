905feb83f2d412cae0aa749e840e5592c7c25d16
kdmontero <kevindmontero@gmail.com>
2022-10-08 12:58:12 +0800

    modified routes to include navbar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,35 @@
 import { Route, Routes } from "react-router-dom"
 
 import { Home, Login, Quotations } from "../pages"
+import { Navbar } from "../components"
 
 const ProjectRoutes = () => {
+  const completePage = (page: JSX.Element) => {
+    return(
+      <div>
+        <Navbar/>
+        {page}
+      </div>
+    )
+  }
+
   return (
     <Routes>
       <Route
         path="/"
-        element={<Home />}
+        element={completePage(<Home />)}
       />
 
       <Route
         path="/quotations"
-        element={<Quotations />}
+
+        element={completePage(<Quotations />)}
       />
 
 
       <Route
         path="/login"
-        element={<Login />}
+        element={completePage(<Login />)}
       />
     </Routes>
   )
