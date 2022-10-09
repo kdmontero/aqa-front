29f8383c2b3af59be19d41fe4afbb8ea4e23744f
kdmontero <kevindmontero@gmail.com>
2022-10-09 22:51:58 +0800

    unfinished implementation of showAllQuote

--- src/pages/Quotations/index.tsx
+++ src/pages/Quotations/index.tsx
@@ -1,7 +1,38 @@
+import { useState } from "react"
+import { getAllQuotations } from "../../api/quotations"
+
+
+
 const Quotations = () => {
+  const [quotations, setQuotations] = useState([])
+
+  const showAllQuotes = () => {
+    getAllQuotations(
+      // headers: 
+    )
+    .then(response=>{
+      setQuotations(response.data.results)
+      console.log(quotations)
+      console.log(response)
+
+    })
+    .catch(error=>{
+      console.log("error", error.response)
+      console.log(quotations)
+
+    })
+  }
   return (
     <div>
       This is Quotations Page
+
+      <button
+        type="submit"
+        onClick={(e)=>{
+          e.preventDefault()
+          showAllQuotes()
+        }}
+      >Show all quotations</button>
     </div>
   )
 }
